55033f527fe014349c8fd3b18a0cdb472fe5e6eb
Théo Gomichon <theo.gom.2000@gmail.com>
2024-12-08 21:06:13 +0100

    feat(2024): add day 1-3

---
+++ 2024/puzzle2/main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getInputPath(day int) string {
+	return fmt.Sprintf("inputs/input%d.txt", day)
+}
+
+func getInput(day int) string {
+	filePath := getInputPath(day)
+	data, err := os.ReadFile(filePath)
+
+	checkErr(err)
+
+	return string(data)
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+
+	return x
+}
+
+func parseInt(s string) int {
+	integer, err := strconv.Atoi(s)
+
+	checkErr(err)
+
+	return integer
+}
+
+func remove(slice []int, removedIndex int) []int {
+	sliceCopy := make([]int, len(slice)-1)
+	sliceCopyIndex := 0
+
+	for sliceIndex, value := range slice {
+		if sliceIndex == removedIndex {
+			continue
+		}
+
+		sliceCopy[sliceCopyIndex] = value
+		sliceCopyIndex++
+	}
+
+	return sliceCopy
+}
+
+func parseReport(input string) []int {
+	levels := strings.Split(input, " ")
+	report := make([]int, len(levels))
+
+	for i, level := range levels {
+		report[i] = parseInt(level)
+	}
+
+	return report
+}
+
+func isReportSafeWithError(report []int, toleratedErrorCount int, errorIndex int) bool {
+	if toleratedErrorCount == 0 {
+		return false
+	}
+
+	// Removing the first level can change the order direction
+	if errorIndex == 1 && isReportSafe(remove(report, errorIndex-1), toleratedErrorCount-1) {
+		return true
+	}
+
+	return isReportSafe(remove(report, errorIndex), toleratedErrorCount-1) || isReportSafe(remove(report, errorIndex+1), toleratedErrorCount-1)
+}
+
+func isReportSafe(report []int, toleratedErrorCount int) bool {
+	if len(report) < 2 {
+		return true
+	}
+
+	increasing := report[0] < report[1]
+
+	for i := 0; i < len(report)-1; i++ {
+		if increasing && report[i] >= report[i+1] {
+			return isReportSafeWithError(report, toleratedErrorCount, i)
+		} else if !increasing && report[i] <= report[i+1] {
+			return isReportSafeWithError(report, toleratedErrorCount, i)
+		}
+
+		difference := abs(report[i] - report[i+1])
+
+		if difference < 1 || difference > 3 {
+			return isReportSafeWithError(report, toleratedErrorCount, i)
+		}
+	}
+
+	return true
+}
+
+func partOne(day int) int {
+	input := getInput(day)
+	lines := strings.Split(input, "\r\n")
+	safeReportCount := 0
+
+	for _, line := range lines {
+		report := parseReport(line)
+
+		if isReportSafe(report, 0) {
+			safeReportCount += 1
+		}
+	}
+
+	return safeReportCount
+}
+
+func partTwo(day int) int {
+	input := getInput(day)
+	lines := strings.Split(input, "\r\n")
+	safeReportCount := 0
+
+	for _, line := range lines {
+		report := parseReport(line)
+
+		if isReportSafe(report, 1) {
+			safeReportCount += 1
+		}
+	}
+
+	return safeReportCount
+}
+
+func main() {
+	day := 2
+
+	partOne := partOne(day)
+	fmt.Printf("Part one: %d\n", partOne)
+
+	partTwo := partTwo(day)
+	fmt.Printf("Part two: %d\n", partTwo)
+}
---
+++ 2024/puzzle3/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+var mulRegex = regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+var doReversedRegex = regexp.MustCompile(`\)\(od`)
+var dontReversedRegex = regexp.MustCompile(`\)\(t'nod`)
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getInputPath(day int) string {
+	return fmt.Sprintf("inputs/input%d.txt", day)
+}
+
+func getInput(day int) string {
+	filePath := getInputPath(day)
+	data, err := os.ReadFile(filePath)
+
+	checkErr(err)
+
+	return string(data)
+}
+
+func parseInt(s string) int {
+	integer, err := strconv.Atoi(s)
+
+	checkErr(err)
+
+	return integer
+}
+
+func mul(instruction []string) int {
+	return parseInt(instruction[1]) * parseInt(instruction[2])
+}
+
+func reverse(s string) string {
+	reversed := []rune(s)
+
+	for i, j := 0, len(reversed)-1; i < j; i, j = i+1, j-1 {
+		reversed[i], reversed[j] = reversed[j], reversed[i]
+	}
+
+	return string(reversed)
+}
+
+func partOne(day int) int {
+	input := getInput(day)
+	regex := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	instructions := regex.FindAllStringSubmatch(input, -1)
+
+	result := 0
+
+	for _, instruction := range instructions {
+		result += mul(instruction)
+	}
+
+	return result
+}
+
+func partTwo(day int) int {
+	input := getInput(day)
+	reversedInput := reverse(input)
+
+	cursor := 0
+	var reversedCursor int
+
+	result := 0
+
+	for {
+		mulIndex := mulRegex.FindStringIndex(input[cursor:])
+
+		if mulIndex == nil {
+			break
+		}
+
+		reversedCursor = len(input) - 1 - cursor - mulIndex[1]
+
+		if reversedCursor < 0 {
+			break
+		}
+
+		doIndex := doReversedRegex.FindStringIndex(reversedInput[reversedCursor:])
+		dontIndex := dontReversedRegex.FindStringIndex(reversedInput[reversedCursor:])
+
+		if dontIndex == nil || (doIndex != nil && doIndex[0] < dontIndex[0]) {
+			instruction := mulRegex.FindStringSubmatch(input[cursor:])
+
+			if instruction != nil {
+				result += mul(instruction)
+			}
+		}
+
+		cursor += mulIndex[1]
+	}
+
+	return result
+}
+
+func main() {
+	day := 3
+
+	partOne := partOne(day)
+	fmt.Printf("Part one: %d\n", partOne)
+
+	partTwo := partTwo(day)
+	fmt.Printf("Part two: %d\n", partTwo)
+}
